ecaf92ed95c116dbacafdd4007fdf13cec5bc035
IoT Lab <verticals.iot.lab@gmail.com>
2024-04-21 19:05:02 +0200

    add filter tab inputs, labels and design

--- src/app/home/filter-tab/filter-tab.component.ts
+++ src/app/home/filter-tab/filter-tab.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-filter-tab',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgxSliderModule],
   templateUrl: './filter-tab.component.html',
   styleUrls: ['./filter-tab.component.css']
 })
 export class FilterTabComponent {
-
+  minValue: number = 100;
+  maxValue: number = 400;
+  options: Options = {
+    floor: 0,
+    ceil: 500
+  };
+  filterByName: string = '';
+  showFavorites: boolean = false;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterTabComponent } from './filter-tab/filter-tab.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BeerListComponent, FilterTabComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
